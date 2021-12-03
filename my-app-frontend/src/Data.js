94a206ae10b2c71debaa7522e726ad899daf56fd
Nancy Zaky <nancyzki@yahoo.com>
2021-12-02 17:41:08 -0800

    changed links

--- my-app-frontend/src/Data.js
+++ my-app-frontend/src/Data.js
@@ -1,13 +1,7 @@
 import React from "react";
 import { RiHandbagFill, RiHeartLine } from "react-icons/ri";
 import { BsPerson } from "react-icons/bs";
-import { AiOutlineShoppingCart } from "react-icons/ai";
-import {
-  GiShirt,
-  GiConverseShoe,
-  GiPearlNecklace,
-  GiLips,
-} from "react-icons/gi";
+
 import { BiTrendingUp } from "react-icons/bi";
 import { FaHome } from "react-icons/fa";
 export const Data = [
@@ -17,40 +11,35 @@ export const Data = [
 
 export const Links = [
   {
-    id: 2,
-    // icon: <GiShirt className="icon" style={{ color: "#7eb1b3" }} />,
+    id: 1,
     url: "/clothes",
     text: "Clothing",
   },
   {
-    id: 3,
-    // icon: <GiConverseShoe className="icon" style={{ color: "#7eb1b3" }} />,
-    url: "/shoes",
-    text: "Shoes",
+    id: 2,
+    url: "/",
+    text: "Night Wear",
   },
   {
-    id: 4,
-    // icon: <GiPearlNecklace className="icon" style={{ color: "#7eb1b3" }} />,
+    id: 3,
     url: "/",
     text: "Accessories",
   },
   {
-    id: 5,
-    // icon: <BiTrendingUp className="icon" style={{ color: "#7eb1b3" }} />,
+    id: 4,
     url: "/",
-    text: "Trending Now",
+    text: "Trending",
   },
+  {
+    id: 5,
+    url: "/",
+    text: "Leather",
+  },
+
   {
     id: 6,
-    // icon: <GiLips className="icon" style={{ color: "#7eb1b3" }} />,
-    url: "/body",
-    text: "Face + Body",
-  },
-  {
-    id: 7,
-    // icon: <GiLips className="icon" style={{ color: "#7eb1b3" }} />,
-    url: "/decoration",
-    text: "Decoration&furniture",
+    url: "/shoes",
+    text: "Shoes",
   },
 ];
 
@@ -58,87 +47,146 @@ export const pageslinks = [
   {
     id: 1,
     page: "Clothing",
-    allpages: ["all", "tops", "shirtblouses", "trousers", "dresses", "jeans"],
+    allpages: [
+      "ladies_tops",
+      "ladies_shirtsblouses",
+      "ladies_trousers",
+      "ladies_dresses",
+      "ladies_loungewear",
+      "ladies_basics",
+      "ladies_jeans",
+      "ladies_shorts",
+      "ladies_skirts",
+      "ladies_nightwear",
+      "ladies_jacketscoats",
+      "ladies_blazerswaistcoats",
+    ],
   },
   {
     id: 2,
     page: "Accessories",
     allpages: [
-      "all",
-      "bags",
-      "belts",
-      "jewellery",
-      "hairaccessories",
-      "sunglasses",
-      "gloves",
-      "hats",
+      "ladies_accessories_jewellery",
+      "ladies_accessories_hairaccessories",
+      "ladies_accessories_sunglasses",
+      "ladies_accessories_hats",
     ],
   },
   {
     id: 3,
     page: "Shoes",
     allpages: [
-      "all",
-      "highheels",
-      "flats",
-      "sandals",
-      "sneakers",
-      "boots",
-      "slipon",
+      "ladies_shoes_pumps_highheels",
+      "ladies_shoes_ballerinas_flats",
+      "ladies_shoes_sandals_espandrillos",
+      "ladies_shoes_sneakers",
+      "ladies_shoes_boots",
+      "Ladies_shoes_slipon",
     ],
   },
   {
     id: 4,
     page: "Home",
-    allpages: ["all", "tops", "shirtblouses", "trousers", "dresses"],
+    allpages: ["all"],
   },
   {
     id: 5,
-    page: "Trending Now",
-    allpages: ["all", "tops", "shirtblouses", "trousers", "dresses"],
+    page: "Trending",
+    allpages: [
+      "ladies_newarrivals_clothes",
+      "ladies_newarrivals_shoesacc",
+      "ladies_newarrivals_swimwear",
+    ],
   },
   {
     id: 6,
-    page: "Face + Body",
-    allpages: ["all", "skincare", "bodywash", "scrubs", "haircare"],
+    page: "Night Wear",
+    allpages: ["ladies_lingerie", "ladies_loungewear", "ladies_nightwear"],
   },
   {
     id: 7,
-    page: "Decoration&furniture",
-    allpages: ["all", "", "", "", ""],
+    page: "Leather",
+    allpages: [
+      "ladies_accessories_bags",
+      "ladies_accessories_gloves",
+      "ladies_accessories_belts",
+    ],
   },
 ];
 
-// ladies_newarrivals_all
-// ladies_newarrivals
-// ladies_newarrivals_clothes
-// ladies_newarrivals_shoesacc;
-// ladies_newarrivals_swimwear
-
-// ladies_all;
-// ladies_trendconcept;
-// ladies_linen;
-// ladies_trousers_wideleg;
-// ladies_modernclassic_shirtsblouse;
-// ladies_modernclassic_blazers;
-// ladies_premium_selection;
-// ladies_tops;
-// ladies_shirtsblouses;
-// ladies_trousers;
-// ladies_dresses;
-// ladies_loungewear;
-// ladies_basics;
-// ladies_jeans;
-// ladies_shorts;
-// ladies_skirts;
-// ladies_nightwear;
-// ladies_lingerie;
-// ladies_jacketscoats;
-// ladies_blazerswaistcoats;
-
-// ladies_shoes_pumps_highheels;
-// ladies_shoes_ballerinas_flats
-// ladies_shoes_sandals_espandrillos;
-// ladies_shoes_sneakers;
-// ladies_shoes_boots;
-// Ladies_shoes_slipon;
+export const pagesMenlinks = [
+  {
+    id: 1,
+    page: "Clothing",
+    allpages: [
+      "men_basics",
+      "men_tshirtstanks",
+      "men_shirts",
+      "men_trousers",
+      "men_shorts",
+      "men_hoodiessweatshirts",
+      "men_nightwearloungewear",
+      "men_sport",
+      "men_jeans",
+      "cardigans-and-jumpers",
+      "men_blazerssuits",
+      "men_jacketscoats",
+    ],
+  },
+  {
+    id: 2,
+    page: "Accessories",
+    allpages: [
+      "men_accessories_ties_bowties_handkerchiefs",
+      "men_accessories_sunglasses",
+      "men_accessories_jewellery",
+      "men_accessories_sportsaccessories",
+      "men_accessories_hatscaps",
+    ],
+  },
+  {
+    id: 3,
+    page: "Shoes",
+    allpages: [
+      "men_shoes_sneakers",
+      "men_shoes_sandals_espandrillos",
+      "men_shoes_boots",
+      "men_shoes_dressed",
+      "men_shoes_loafers",
+      ,
+    ],
+  },
+  {
+    id: 4,
+    page: "Home",
+    allpages: ["all"],
+  },
+  {
+    id: 5,
+    page: "Trending",
+    allpages: [
+      "men_newarrivals_clothes",
+      "men_newarrivals_shoesaccessories",
+      "men_premium_selection_trousers",
+      "men_premium_selection_shoes",
+      "men_premium_selection_shirts",
+      "men_premium_selection_jacketscoats",
+      "men_premium_selection_cardigansjumpers",
+      "men_premium_selection_accessories",
+    ],
+  },
+  {
+    id: 6,
+    page: "Night Wear",
+    allpages: ["men_nightwearloungewear", "men_swimweear", "men_socks"],
+  },
+  {
+    id: 7,
+    page: "Leather",
+    allpages: [
+      "men_accessories_bags",
+      "men_accessories_gloves",
+      "men_accessories_beltsandbraces",
+    ],
+  },
+];
